e8e2e713b39133e5dd566a933e901183e7a84b7e
ilko.garcia <ilko.garcia@gmail.com>
2023-02-14 17:57:31 +0100

    Add tasting menu page

--- js/reserv.js
+++ js/reserv.js
@@ -15,52 +15,75 @@ const showMessage = (textMsg) => {
         showElement('message-popup');
 }
 
+// Pop Up - show message passed in parameters 
+const setSuccessMessage = (textMsg) => {
+    if (textMsg == 'OK') {
+        document.getElementById("messagepopup-title").innerHTML = "GRACIAS";
+        document.getElementById("messagepopup-text").innerHTML = "Su reserva ha sido enviada con éxito";
+        showElement('message-popup');
+    } else {
+        document.getElementById("messagepopup-title").innerHTML = "ALERTA";
+        document.getElementById("messagepopup-text").innerHTML = "Ha ocurrido un error durante el envío. Compruebe los datos que ha introducido como por ejemplo que su dirección de correo electrónico sea valida.";
+        showElement('message-popup');
+    }
+}
+
 const isValidName = () => {
     let name = document.getElementById("inputName").value;
-    return (name == "" || name == null) ? false : true;
+    return (name != "" || name != null) ? true : false;
 }
 
 const isValidPhone = () => {
-    // Validate a phone number of 9 digits with no comma, no spaces, no punctuation and no + sign in front the number.
-    let pattern1 = /^\d{9}$/;
-    // Valid a phone number like XXX-XXX-XXXX, XXX.XXX.XXXX or XXX XXX XXXX
-    // let pattern2 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    // Valid a phone number like +XX-XXXX-XXXX, +XX.XXXX.XXXX, or +XX XXXX XXXX
-    // let pattern3 = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-
     let phone = document.getElementById("inputPhone").value;
-    if (phone.match("/^\d{9}$/")) {
-        return true;
-    } else return true;
+    return (phone != "" || phone != null ) ? true : false;
 }
 
 const isValidPax = () => {
     let pax = document.getElementById("inputPax").value;
-
+    return (pax >= 2 && pax <= 12) ? true : false;
 }
 
 const isValidDate = () => {
-    let date = document.getElementById("inputDate").value;
-
+    // let userdate = document.getElementById("inputDate").value;
+    // const date = new Date();
+    // let day = date.getDate();
+    // let month = date.getMonth() + 1;
+    // let year = date.getFullYear();
+    // let currentDate = `${year}-${month}-${day}`;
+    // (userdate.parse() > currentDate.parse()) ? true : false;
+    // console.log(userdate);
+    // console.log(date);
+    // console.log(currentDate);
+return true;
 }
 
-const isValidTurn = () => {
+// Function to send emails via SmtpJS.com
+const sendEmail = () => {
+    let name = document.getElementById("inputName").value;
+    let phone = document.getElementById("inputPhone").value;
+    let pax = document.getElementById("inputPax").value;
+    let date = document.getElementById("inputDate").value;
     let turn = document.getElementById("inputTurn").value;
 
+    Email.send({
+        SecureToken : "cff0dcc0-27c7-4251-ae99-c60dad645fbd",
+        To : 'reservas@viejobarrio.es',
+        From : "maximo@4vientos.com",
+        Subject : "Reserva de: " + name,
+        Body : "Nombre:" + name + "Teléfono:" + phone + "Comensales:" + pax + "Dia:" + date + "Turno:" + turn
+    })
+    .then(response => setSuccessMessage(response));
 }
 
 // Method to validate the booking form
 const valBookingData = () => {
-
-    if (isValidName()) {
-        showMessage("El nombre es correcto")
-    } else {
+    if (isValidName() != true) {
         showMessage("Es necesario que indiques un nombre")
-    }
-
-    if (isValidPhone()) {
-        showMessage("El teléfono es correcto")
-    } else {
+    } else if (isValidPhone() != true) {
         showMessage("Es necesario que indiques un número de teléfono correcto")
+    } else if (isValidPax() != true) {
+        showMessage("Solo es posibe realizar reservas para 1 a 12 comensales")
+    } else {
+        sendEmail();
     }
 }
